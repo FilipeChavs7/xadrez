78e9b704867c83246fec462e874ccfbad24e68a1
Filipe Chaves <filipechavs7@gmail.com>
2021-06-21 11:13:54 -0300

    Imprimir peca com cor

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -14,7 +14,8 @@ namespace xadrez_console
                 Tabuleiro tab = new Tabuleiro(8, 8);
                 tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
                 tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-                tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(0,9));
+                tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(3,4));
+                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(6, 3));
 
 
 
