0d5908f5a0c7bffaf237ef75dbda309620e1352f
Filipe Chaves <filipechavs7@gmail.com>
2021-06-22 10:44:19 -0300

    Implementando xequemate

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -9,45 +9,42 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            try
+
+            PartidadeXadrez partida = new PartidadeXadrez();
+            while (!partida.terminada)
             {
-                PartidadeXadrez partida = new PartidadeXadrez();
-                while (!partida.terminada)
+                try
                 {
-                    try
-                    {
-                        Console.Clear();
-                        Tela.imprimirPartida(partida);
-                        
+                    Console.Clear();
+                    Tela.imprimirPartida(partida);
 
-                        Console.WriteLine();
-                        Console.Write("Origem: ");
-                        Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
-                        partida.validarPosicaoDeOrigem(origem);
-                        bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
 
-                        Console.Clear();
-                        Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
-                        Console.WriteLine();
-                        Console.Write("Destino: ");
-                        Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
-                        partida.validarPosicaoDeDestino(origem, destino);
+                    Console.WriteLine();
+                    Console.Write("Origem: ");
+                    Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+                    partida.validarPosicaoDeOrigem(origem);
+                    bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
 
-                        partida.realizaJogada(origem, destino);
-                    }
-                    catch (TabuleiroException e)
-                    {
-                        Console.WriteLine(e.Message);
-                        Console.ReadLine();
-                    }
-                    Tela.imprimirTabuleiro(partida.tab);
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
+                    Console.WriteLine();
+                    Console.Write("Destino: ");
+                    Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+                    partida.validarPosicaoDeDestino(origem, destino);
+
+                    partida.realizaJogada(origem, destino);
+                }
+                catch (TabuleiroException e)
+                {
+                    Console.WriteLine(e.Message);
+                    Console.ReadLine();
                 }
             }
-            catch (TabuleiroException e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            Console.Clear();
+            Tela.imprimirPartida(partida);
             Console.ReadLine();
         }
+
     }
 }
+
